0cb045f0f07b5563c77047664df3e13458d226ae
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-04-26 22:23:01 +0530

    Update QTReverseString.s

--- QTReverseString.s
+++ QTReverseString.s
@@ -1,6 +1,8 @@
-.data
-prompt: .asciiz "\n Enter the string \n" str: .space 32
+.data 
+prompt: .asciiz  "\n Enter the string \n"
+str: .space 32
 res: .asciiz "\n The reversed string is \n"
+
 .globl main
 .text
 main:
@@ -13,17 +15,18 @@ syscall
 li $v0,4
 la $a0,res
 syscall
+
 li $t2,0
-strLen:
-lb $t0, str($t2)
+
+strLen:                 
+lb $t0, str($t2)  
 add $t2, $t2, 1
 bne $t0, $zero, strLen
-li $v0, 11
+li $v0, 11         
+
 Loop:
 sub $t2, $t2, 1
-la $t0, str($t2)
+la $t0, str($t2)  
 lb $a0, ($t0)
 syscall
 bnez $t2, Loop
-li $v0, 10
-syscall
